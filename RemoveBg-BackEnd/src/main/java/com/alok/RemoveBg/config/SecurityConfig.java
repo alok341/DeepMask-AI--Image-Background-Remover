341100bd094b1987ebb0a69e2ca584b8bb96747f
Alokkumar Ajaykumar Dubey <dubeyalokkumar2005@gmail.com>
2026-03-08 16:30:26 +0530

    Update SecurityConfig.java

--- RemoveBg-BackEnd/src/main/java/com/alok/RemoveBg/config/SecurityConfig.java
+++ RemoveBg-BackEnd/src/main/java/com/alok/RemoveBg/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://deep-mask-ai-image-background-remov.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
